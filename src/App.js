76724655852cd15ae740006520828bcae01a6cee
unknown <ajdourelhass2096@gmail.com>
2022-10-29 03:00:44 +0200

    add all files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Menu from "./components/Layout/Menu";
+import Footer from "./components/Layout/Footer";
+import Showcase from "./components/Layout/Showcase";
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      <Showcase />
+      <Footer  />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <div>
+      <div>
+        <h2> Portfolio </h2>
+      </div>
+      <div>
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Menu
+
